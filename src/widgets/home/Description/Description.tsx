13bfc086828c8e60c7cebc39ca4f1260d83aab33
Shenho Parallux <anovikova999666@gmail.com>
2025-07-24 22:30:22 +0300

    Banner/Сделал секцию desctiption без адаптива

--- src/widgets/home/Description/Description.tsx
+++ src/widgets/home/Description/Description.tsx
@@ -1,12 +1,34 @@
 import "./Description.scss";
+import { useTranslation } from "react-i18next";
 
 export const Description = () => {
+  const { t } = useTranslation("home");
   return (
     <section className={"description"}>
       <div className="description__inner container">
-        <ul className="description-list">
-          <li className="description-item"></li>
-        </ul>
+        <div className="description-info">{t("description.reliability")}</div>
+        <div className="description__body">
+          <ul className="description__body-list">
+            <li className="description__body-item">
+              <div className="description__body-icon"></div>
+              <div className="description__body-text">
+                {t("description.text_1")}
+              </div>
+            </li>
+            <li className="description__body-item">
+              <div className="description__body-icon"></div>
+              <div className="description__body-text">
+                {t("description.text_2")}
+              </div>
+            </li>
+            <li className="description__body-item">
+              <div className="description__body-icon"></div>
+              <div className="description__body-text">
+                {t("description.text_3")}
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );

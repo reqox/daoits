a2af41ee2619578e3523683b77e360b9bdef53c8
Shenho Parallux <anovikova999666@gmail.com>
2025-07-24 23:08:26 +0300

    Сделал routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "@/app/Layout/Layout.tsx";
-import { CoopPage, HomePage } from "@/pages";
+import { AppPage, CoopPage, HomePage, ServicesPage } from "@/pages";
 import { useTheme } from "@/shared/hooks";
 import "./App.scss";
 import "@/shared/styles/main.scss";
@@ -12,6 +12,21 @@ const App = () => {
       <Route path={"/"} element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path={"coop"} element={<CoopPage />} />
+        <Route path={"services"} element={<ServicesPage />}>
+          <Route path={"app"} element={<AppPage />} />
+          <Route path={"web"} element={<AppPage />} />
+          <Route path={"promotion"} element={<AppPage />} />
+        </Route>
+
+        <Route path={"developments"} element={<AppPage />}>
+          <Route path={"defibridge"} element={<AppPage />} />
+          <Route path={"economy"} element={<AppPage />} />
+          <Route path={"feonyx"} element={<AppPage />} />
+        </Route>
+        <Route path={"tokenomics"} element={<AppPage />}>
+          <Route path={"ifehu"} element={<AppPage />} />
+          <Route path={"investor"} element={<AppPage />} />
+        </Route>
 
         <Route path={"*"} element={<Navigate to={"/"} replace={true} />} />
       </Route>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,4 @@
 export { HomePage } from "./HomePage/HomePage";
 export { CoopPage } from "./CoopPage/CoopPage";
+export { AppPage } from "./AppPage/AppPage";
+export { ServicesPage } from "./ServicesPage/ServicesPage";

318454c005eca88a58b62cf530252c49fd2cff1b
Volshebnik <volshebnik7117@gmail.com>
2025-07-29 17:11:57 +0300

    Начинаю адаптив сделанных секций

--- src/widgets/home/Description/Description.tsx
+++ src/widgets/home/Description/Description.tsx
@@ -1,31 +1,25 @@
-import "./Description.scss";
-import { useTranslation } from "react-i18next";
+import './Description.scss';
+import { useTranslation } from 'react-i18next';
 
 export const Description = () => {
-  const { t } = useTranslation("home");
+  const { t } = useTranslation('home');
   return (
-    <section className={"description"}>
+    <section className={'description'}>
       <div className="description__inner container">
-        <div className="description-info">{t("description.reliability")}</div>
+        <div className="description-info">{t('description.reliability')}</div>
         <div className="description__body">
           <ul className="description__body-list">
             <li className="description__body-item">
               <div className="description__body-icon"></div>
-              <div className="description__body-text">
-                {t("description.text_1")}
-              </div>
+              <div className="description__body-text">{t('description.text_1')}</div>
             </li>
             <li className="description__body-item">
               <div className="description__body-icon"></div>
-              <div className="description__body-text">
-                {t("description.text_2")}
-              </div>
+              <div className="description__body-text">{t('description.text_2')}</div>
             </li>
             <li className="description__body-item">
               <div className="description__body-icon"></div>
-              <div className="description__body-text">
-                {t("description.text_3")}
-              </div>
+              <div className="description__body-text">{t('description.text_3')}</div>
             </li>
           </ul>
         </div>

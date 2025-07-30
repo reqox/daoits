7b4c92f91c2c10e1f5119fdb12a323aef7f20fd5
Volshebnik <volshebnik7117@gmail.com>
2025-07-30 20:06:22 +0300

    Сделал секцию Mission

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,13 @@
 import './HomePage.scss';
 import { Fragment } from 'react';
-import { Banner, Description } from '@/widgets/home';
+import { Banner, Description, Mission } from '@/widgets/home';
 
 export const HomePage = () => {
   return (
     <Fragment>
       <Banner />
       <Description />
+      <Mission />
     </Fragment>
   );
 };

131563cda63e2d511c09aa0f228fc9eeb0bedd9a
Volshebnik <volshebnik7117@gmail.com>
2025-07-30 20:06:22 +0300

    Сделал секцию Mission

---
+++ src/shared/ui/InfoCard/InfoCard.tsx
@@ -0,0 +1,16 @@
+import type { FC } from 'react';
+import './InfoCard.scss';
+
+interface InfoCardProps {
+  title: string;
+  body: string;
+}
+
+export const InfoCard: FC<InfoCardProps> = ({ title, body }) => {
+  return (
+    <div className="infocard">
+      <h3 className="infocard-title">{title}</h3>
+      <div className="infocard-body">{body}</div>
+    </div>
+  );
+};
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,6 @@
-export { SwitchTheme } from "./SwitchTheme/SwitchTheme";
-export { SwitchLanguage } from "./SwitchLanguage/SwitchLanguage";
-export { NavigationMenu } from "./NavigationMenu/NavigationMenu";
-export { BurgerButton } from "./BurgerButton/BurgerButton";
-export { BurgerMenu } from "./BurgerMenu/BurgerMenu";
+export { SwitchTheme } from './SwitchTheme/SwitchTheme';
+export { SwitchLanguage } from './SwitchLanguage/SwitchLanguage';
+export { NavigationMenu } from './NavigationMenu/NavigationMenu';
+export { BurgerButton } from './BurgerButton/BurgerButton';
+export { BurgerMenu } from './BurgerMenu/BurgerMenu';
+export { InfoCard } from './InfoCard/InfoCard';

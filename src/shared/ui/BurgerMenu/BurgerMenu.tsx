7c606f8c12740444134e3f4dfaf9c0eebc1f3a47
Volshebnik <volshebnik7117@gmail.com>
2025-07-30 22:05:17 +0300

    bagfix/Исправил баг с отображением бургер меню

--- src/shared/ui/BurgerMenu/BurgerMenu.tsx
+++ src/shared/ui/BurgerMenu/BurgerMenu.tsx
@@ -1,10 +1,10 @@
-import "./BurgerMenu.scss";
-import * as React from "react";
-import { useEffect, useRef } from "react";
-import { NavLink } from "react-router-dom";
-import { LogoSvg } from "@/shared/assets/icons";
-import { NavigationMenu, SwitchLanguage, SwitchTheme } from "@/shared/ui";
-import { useTranslation } from "react-i18next";
+import './BurgerMenu.scss';
+import * as React from 'react';
+import { useEffect, useRef } from 'react';
+import { NavLink } from 'react-router-dom';
+import { LogoSvg } from '@/shared/assets/icons';
+import { NavigationMenu, SwitchLanguage, SwitchTheme } from '@/shared/ui';
+import { useTranslation } from 'react-i18next';
 
 interface iBurgerMenu {
   isOpen: boolean;
@@ -12,20 +12,17 @@ interface iBurgerMenu {
 }
 
 export const BurgerMenu: React.FC<iBurgerMenu> = ({ isOpen, f }) => {
-  const { t } = useTranslation("header");
+  const { t } = useTranslation('header');
   const bodyRef = useRef(document.body);
   useEffect(() => {
     const style = bodyRef.current.style;
     if (isOpen) {
-      style.height = "100svh";
-      style.overflow = "hidden";
+      style.overflow = 'hidden';
     } else {
-      style.height = "auto";
-      style.overflow = "auto";
+      style.overflow = 'auto';
     }
     return () => {
-      style.height = "auto";
-      style.overflow = "auto";
+      style.overflow = 'auto';
     };
   }, [isOpen]);
 
@@ -33,12 +30,12 @@ export const BurgerMenu: React.FC<iBurgerMenu> = ({ isOpen, f }) => {
 
   return (
     <div
-      id={"mobile-menu"}
-      className={"menu visible-tablet"}
+      id={'mobile-menu'}
+      className={'menu visible-tablet'}
       aria-hidden={!isOpen}
     >
       <div className="menu__inner">
-        <NavLink to={"/"} className="menu__logo">
+        <NavLink to={'/'} className="menu__logo">
           <img
             className="menu__logo-image"
             src={LogoSvg}
@@ -50,17 +47,17 @@ export const BurgerMenu: React.FC<iBurgerMenu> = ({ isOpen, f }) => {
           />
         </NavLink>
         <div className="menu__main">
-          <div className="menu__main-title">{t("burger.menu.navigation")}</div>
+          <div className="menu__main-title">{t('burger.menu.navigation')}</div>
           <NavigationMenu f={f} />
         </div>
         <div className="menu__extra">
           <div className="menu__settings">
             <div className="menu__settings-title">
-              {t("burger.menu.settings")}
+              {t('burger.menu.settings')}
             </div>
             <div className="menu__settings__language">
               <div className="menu__settings__language-info">
-                {t("burger.menu.language")}
+                {t('burger.menu.language')}
               </div>
               <div className="menu__settings__language-main">
                 <SwitchLanguage />
@@ -68,7 +65,7 @@ export const BurgerMenu: React.FC<iBurgerMenu> = ({ isOpen, f }) => {
             </div>
             <div className="menu__settings__theme">
               <div className="menu__settings__settings-info">
-                {t("burger.menu.theme")}
+                {t('burger.menu.theme')}
               </div>
               <div className="menu__settings__settings-main">
                 <SwitchTheme />
@@ -79,17 +76,17 @@ export const BurgerMenu: React.FC<iBurgerMenu> = ({ isOpen, f }) => {
             <a
               href={`https://t.me/${import.meta.env.VITE_TELEGRAM}`}
               className="menu__telegram-link"
-              target={"_blank"}
+              target={'_blank'}
             >
               <span className="menu__telegram-link-text">
-                {t("burger.menu.telegram")}
+                {t('burger.menu.telegram')}
               </span>
             </a>
           </div>
           <div className="menu__contact-us">
             <a href="#contact-us" className="menu__contact-us-link">
               <span className="menu__contact-us-link-text">
-                {t("burger.menu.contactUs")}
+                {t('burger.menu.contactUs')}
               </span>
             </a>
           </div>

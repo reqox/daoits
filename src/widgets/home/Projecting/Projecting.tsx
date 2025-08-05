e35546fb1361a5e97bcf2496c5bd2e961951da8b
Shenho Parallux <anovikova999666@gmail.com>
2025-08-05 23:39:07 +0300

    Сделал перевод секции Projecting

--- src/widgets/home/Projecting/Projecting.tsx
+++ src/widgets/home/Projecting/Projecting.tsx
@@ -1,18 +1,18 @@
 import { InfoCard } from '@/shared/ui';
 import './Projecting.scss';
+import { useTranslation } from 'react-i18next';
 
 export const Projecting = () => {
+  const { t } = useTranslation('home');
   return (
     <section className={'projecting'}>
       <div className="projecting__inner container">
-        <h2 className="projecting-title">
-          Проектирование децентрализованного мира
-        </h2>
-        <InfoCard body="Повысим информационную безопасность компании и сотрудников" />
-        <InfoCard body="Автоматизируем рабочие процессы смартконтрактами" />
-        <InfoCard body="Перенесем ваш бизнес в блокчейн среду" />
-        <InfoCard body="Анонимно и безопасно купим/продадим для вас цифровые финансовые активы" />
-        <InfoCard body="Консультации профессиональных аналитиков, юристов и веб-специалистов" />
+        <h2 className="projecting-title">{t('projecting.title')}</h2>
+        <InfoCard body={t('projecting.card.1')} />
+        <InfoCard body={t('projecting.card.2')} />
+        <InfoCard body={t('projecting.card.3')} />
+        <InfoCard body={t('projecting.card.4')} />
+        <InfoCard body={t('projecting.card.5')} />
       </div>
     </section>
   );

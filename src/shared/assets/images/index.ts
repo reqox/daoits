fcaeb0c875512d4de85857e8b1570de09ffb33b3
Shenho Parallux <anovikova999666@gmail.com>
2025-08-06 13:01:29 +0300

    сделал секцию services

--- src/shared/assets/images/index.ts
+++ src/shared/assets/images/index.ts
@@ -1,4 +1,13 @@
 import BannerImage from './banner/background.png';
 import DescriptionChartImage from './description/chart.svg';
+import Services1Image from './services/1.png';
+import Services2Image from './services/2.png';
+import Services3Image from './services/3.png';
 
-export { BannerImage, DescriptionChartImage };
+export {
+  BannerImage,
+  DescriptionChartImage,
+  Services1Image,
+  Services2Image,
+  Services3Image,
+};
